285e934822db16721cfb18b38060d585c02afd91
Michael Steinmejer <mich86p0@edu.easj.dk>
2018-09-14 17:24:25 +0200

    Done

---
+++ StoreBaeltBilletLibrary/Weekend.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using BilletLibrary;
+
+namespace StoreBaeltBilletLibrary
+{
+    public static class Weekend
+    {
+        public static double PrisWeekendRabat(Bil bil)
+        {
+            if (bil.Dato.DayOfWeek == (DayOfWeek) 6 | bil.Dato.DayOfWeek == 0)
+            {
+                return (bil.BrobizzRabat() * 0.80);
+            }
+            return bil.BrobizzRabat();
+        }
+    }
+}
