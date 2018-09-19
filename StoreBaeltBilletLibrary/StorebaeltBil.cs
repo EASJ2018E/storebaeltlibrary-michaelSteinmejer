36f014e6abe265fa8029514d8a881b1d11abfd3c
Michael Steinmejer <mich86p0@edu.easj.dk>
2018-09-19 11:10:13 +0200

    blah

---
+++ StoreBaeltBilletLibrary/StorebaeltBil.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using BilletLibrary;
+
+namespace StoreBaeltBilletLibrary
+{
+    class StorebaeltBil:Transportmiddel
+    {
+        public StorebaeltBil(string nummerplade, DateTime dato, bool brobizz) : base(nummerplade, dato, brobizz)
+        {
+            if (nummerplade.Length<7)
+            {
+                throw new SystemException("broo for real?");
+            }
+        }
+
+        public override double Pris()
+        {
+            if (Brobizz== true && Dato.DayOfWeek == DayOfWeek.Saturday || Dato.DayOfWeek == DayOfWeek.Sunday)
+            {
+                return (240 * 0.95)*0.80;
+            }
+            if (Brobizz== true)
+            {
+                return 240 * 0.95;
+            }
+            if (Dato.DayOfWeek == DayOfWeek.Saturday ||Dato.DayOfWeek == DayOfWeek.Sunday)
+            {
+                return 240 * 0.80;
+            }
+          
+            return 240;
+        }
+
+        public override string Køretøj()
+        {
+            return "Bil";
+        }
+    }
+}
